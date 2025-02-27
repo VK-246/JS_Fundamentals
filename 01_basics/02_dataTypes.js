b6f2410ca9b0ab5378e0e11f972bc473ddf5ed09
VK-246 <vinayakkanhegaonkar5383@gmail.com>
2025-02-27 09:49:06 +0000

    About types,operations and conversions

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all JS code as newer versions
+// alert( 3+3 ) // Reference error alert not defined //does not run on nodejs(diffrent syntax) , only on browser
+// console.log(3+3) console.log(45);
+// syntax error 
+
+// tc39 -> ECMA standards for javascript
+// mdn
+
+// numbers : 2^53 , bigint , string , boolean , null , undefined , symbol 
+console.log(typeof(undefined)); //undefined is undefined
+console.log(typeof(null)); //null is an object
+          
+
+
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// bad practice of writing code even if the code passes
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === : strict comparison
+console.log("2" == 2); // returns true
+console.log("2" === 2); // returns false
