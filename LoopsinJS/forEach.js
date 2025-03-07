66db62022633218523862e52d96249942185e175
VK-246 <vinayakkanhegaonkar5383@gmail.com>
2025-03-07 17:41:14 +0000

    loops

---
+++ LoopsinJS/forEach.js
@@ -0,0 +1,10 @@
+const arr = ['cpp','c','python','js','html','css']
+
+
+Variation - 1
+arr.forEach( function (key) {
+    console.log(key)
+} )
+
+// Variation - 2
+// arr.forEach( (item) => { console.log(item) } )
--- LoopsinJS/loops.js
+++ LoopsinJS/loops.js
@@ -9,31 +9,45 @@
     // }
     
     // 3. do {
-        
+        //runs atleast once
     // } while (condition);
     
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // More loops
-// for of
+// for of picks values of arr elements, has syntax to access map
+// for in gets 0 based indexing for arrays, 
+
+
+//4. for of
 let arr = [1,2,3,5,6]
 for (const element of arr) {
-    console.log(element);
+    // console.log(element);
 }
-
+//5. for in aur for off dono use kr skte he idhar
 const greetings = "hare krishna!"
 for (const pos in greetings) {
-    console.log(`At ${pos}, Char is: ${greetings[pos]}`);
+    // console.log(`At ${pos}, Char is: ${greetings[pos]}`);
 }
 
-// Maps: for of works with below syntax
+// Introduction to map data structure in js
 const map = new Map()
 map.set('IN',"India")
 map.set('USA',"United States")
 map.set('Ger',"Germany")
 // console.log(map);
 
-for (const [key,value] of map) {
-    console.log(`${key} : ${value}`);
+// //Maps: for of works with below syntax
+// for (const key of map) {
+//     console.log(key);
+// }
+// for (const [key,value] of map) {
+//    console.log(`${key} : ${value}`);
+// }
+
+// for in on maps
+for (const key in map) {
+//    console.log(key);
+   
 }
 
 // On Objects: for of does not work
@@ -48,9 +62,7 @@ const myObject = {
 
 // using for in
 for (const key in myObject) {
-    if (Object.prototype.hasOwnProperty.call(myObject, key)) {
-        const element = myObject[key];
-        console.log(element);
-        
-    }
+        // console.log(key); //prints keys
+        // console.log(myObject[key])//prints value
+        // console.log(`${key} is ${myObject[key]}`);   
 }
