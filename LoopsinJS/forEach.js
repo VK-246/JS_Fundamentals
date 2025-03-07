6323277a88e2f288e38de983e1cc0e9c23a9719d
VK-246 <vinayakkanhegaonkar5383@gmail.com>
2025-03-07 18:10:43 +0000

    forEach uses and syntax

--- LoopsinJS/forEach.js
+++ LoopsinJS/forEach.js
@@ -1,10 +1,41 @@
 const arr = ['cpp','c','python','js','html','css']
 
 
-Variation - 1
-arr.forEach( function (key) {
-    console.log(key)
-} )
+// // Variation - 1
+// arr.forEach( function (key) {
+//     console.log(key)
+// } )
 
-// Variation - 2
+// // Variation - 2
 // arr.forEach( (item) => { console.log(item) } )
+
+// // Variation - 3
+// function printMe(item){
+//     ( console.log(item) )
+// }
+// arr.forEach(printMe)
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Parmeters of for each function
+// arr.forEach( function (key,idx,ar) {
+//     console.log(key,idx,ar)
+// } )
+
+// For accessing array of js object data type
+// very useful for practical purposes
+const myCoding = [
+    {
+        LangName: "Python",
+        FileName: 'py'
+    },
+    {
+        LangName: "ruby",
+        FileName: 'rb'
+    }
+]
+
+myCoding.forEach( function(item) {
+     console.log(`Language ${item.LangName} has FileName ${item.FileName}`);
+ } )
