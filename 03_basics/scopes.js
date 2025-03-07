83658cde4ea1d6a715732f5b5d50976d242ce3e1
VK-246 <vinayakkanhegaonkar5383@gmail.com>
2025-03-07 05:06:03 +0000

    minor changes

--- 03_basics/scopes.js
+++ 03_basics/scopes.js
@@ -11,10 +11,10 @@
 // if (true) {
 //     let a = 10
 //     const b = 20
-//      var c = 30 
+//      var c = 30 //mujhe mere aukat me rehna chahia lekin me aukat se bahar aa gya, kyuki var ne uksaya
 // }
 
-// // console.log(a);
+//  //console.log(a); //Reference Error: a not defined
 // console.log(c); // 30
 
 // let c = 300
@@ -56,17 +56,15 @@ if (true) {
 // ++++++++++++++++++ interesting ++++++++++++++++++
 // hoisting and execution context.
 
-console.log(addone(5))
 
+console.log(addone(5)) 
 function addone(num){
     return num + 1
 }
 
-
 // const has a local scope so addTwo is not read in memory phase
 // whereas function is read in memmory phase
-console.log(addTwo(5));
-
+// console.log(addTwo(5));
 // storing function in a variable
 const addTwo = function(num){
     return num + 2
